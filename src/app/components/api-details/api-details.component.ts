2c09aedf27fbd74bd41779e92b0b6f911a2c16f1
NiteeshRavichandran <nits@INDC-PF1409TW>
2024-11-20 12:38:39 +0530

    first phase completed

--- src/app/components/api-details/api-details.component.ts
+++ src/app/components/api-details/api-details.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { HeaderComponent } from "../header/header.component";
 import { MatTabsModule } from '@angular/material/tabs';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import * as swgDef from '../../../assets/swagger_definition.json'
+
 
 @Component({
   selector: 'app-api-details',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, MatTabsModule],
+  imports: [FooterComponent, HeaderComponent, MatTabsModule, NgFor, NgIf, CommonModule],
   templateUrl: './api-details.component.html',
   styleUrl: './api-details.component.css'
 })
 export class ApiDetailsComponent {
 
+  data = swgDef;
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,21 +2,33 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import * as swgDef from '../../../assets/swagger_definition.json'
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatSidenavModule, MatIconModule, MatListModule, NgFor],
+  imports: [MatSidenavModule, MatIconModule, MatListModule, NgFor, RouterModule, NgIf],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent implements OnInit {
   currentYear = new Date().getFullYear();
+  data = swgDef;
+  shflag = false;
+  shflag2 = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  togglefn(){
+    this.shflag = !this.shflag;
+  }
+  togglefn2(){
+    this.shflag2 = !this.shflag2;
   }
 
 }
